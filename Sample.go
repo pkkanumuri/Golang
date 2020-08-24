844a8106107edf366f5a25a4a2e3ab2af0fcde84
pkkanumuri <70136961+pkkanumuri@users.noreply.github.com>
2020-08-24 16:43:07 +0530

    Create Sample.go

---
+++ Sample.go
@@ -0,0 +1,132 @@
+
+// ***constants****
+//package main
+
+//import ("fmt")
+
+//func main(){
+
+//const a = 12
+//const c int64 = 2
+//var b int64 = 23
+//fmt.Printf("%v,%T\n", a + b + c, a + b + c)
+//}
+
+
+
+//***Integers***
+//package main
+//import ("fmt")
+
+//func main(){
+//a := 12
+//b := 2
+//var c int8 = 4
+//fmt.Println(a+b)
+//fmt.Println(a-b)
+//fmt.Println(a*b)
+//fmt.Println(a/b)
+//fmt.Println(a%b)
+//fmt.Println(a%int(c))
+//}
+
+
+
+
+
+//***Boolean***
+//package main
+//import ("fmt")
+
+//func main(){
+//var i bool = false
+//var k bool = true
+//fmt.Printf("%v, %T\n", i, i)
+//fmt.Printf("%v, %T\n", k, k)
+//{
+//n := 1==1
+//fmt.Printf("%v, %T\n", n, n)
+//m := 1==2
+//fmt.Printf("%v, %T\n", m, m)
+//}
+//}
+
+
+
+
+
+//***Variables*** with String
+//package main
+
+//import (
+//	"fmt"
+//)
+
+//var Name string = "pavan"
+
+//func main() { fmt.Println("Hello World")
+//fmt.Println(Name) 
+//var name string = "venu"
+//fmt.Println(name) 
+//var name1 string = "venu1"
+//fmt.Println(name1) 
+
+//{
+//var Name string = "venu2"
+//fmt.Println(Name) 
+//}
+//}
+
+
+
+
+
+
+//***Variables*** with Integer
+//package main
+
+//import (
+//	"fmt"
+//	"strconv"
+//)
+
+//func main() {
+//	fmt.Println("Pavan Kanumuri")
+
+//	var i int = 30
+//	fmt.Println(i)
+
+//	var name string = "pavan"
+//	fmt.Println(name)
+
+//	var s string = strconv.Itoa(i)
+//	fmt.Println(s)
+
+//}
+
+
+
+
+
+//***Variables*** with String conversion
+//package main
+
+//import (
+//	"fmt"
+//	"strconv"
+//)
+
+//func main() {
+//var i int
+//i=45
+//var n int = 44
+//var name string = "pavan"
+
+// m := 85
+//	fmt.Println("Hello, playground")
+//	fmt.Println(i)
+//	fmt.Printf("%v, %T\n", n, n)
+//	var l = strconv.Itoa(n)
+//	fmt.Println(l)
+//	fmt.Println(name)
+//}
